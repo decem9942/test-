adc27ff36985e903a9aa2cbe5d49d9023dd16292
cheng <cheng@decem>
2025-05-26 20:53:22 +0900

    Merge branch 'main' of https://github.com/decem9942/test-.git into main

---
+++ src/test/tset002.java
@@ -0,0 +1,16 @@
+package test;
+
+public class tset002 {
+	public static void main(String[] args){
+		String name = "山田太郎"; 
+		int age = 25;	
+		String hobby = "映画鑑賞";
+				
+			
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
+	}
+	
+}
