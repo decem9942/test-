d94b5f3b72b5d77bb32327731e155f4c7a272574
cheng <cheng@decem>
2025-06-27 12:30:32 +0900

    kadai

---
+++ src/curriculum_A/Qes1_13.java
@@ -0,0 +1,27 @@
+package curriculum_A;
+
+public class main {
+    public static void main(String[] args) {
+
+        {
+            byte byteVar = 0;
+            short shortVar = 0;
+            int intVar = 0;
+            long longVar = 0L;
+        }
+
+        {
+            float floatVar = 0.0f;
+            double doubleVar = 0.0;
+        }
+
+        {
+            char charVar = '\u0000';
+            String stringVar = "";
+        }
+
+        {
+            boolean booleanVar = false;
+        }
+    }
+}
