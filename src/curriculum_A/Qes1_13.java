75b5a8fc8d098892bca25d546ec50ce8b5eb43f7
cheng <cheng@decem>
2025-07-14 22:41:23 +0900

    test

--- src/curriculum_A/Qes1_13.java
+++ src/curriculum_A/Qes1_13.java
@@ -1,27 +1,39 @@
-package curriculum_A;
-
-public class main {
+ public class main {
     public static void main(String[] args) {
 
         {
             byte byteVar = 0;
+            byteVar = 10;
+
             short shortVar = 0;
+            shortVar = 1000;
+
             int intVar = 0;
+            intVar = 100000;
+
             long longVar = 0L;
+            longVar = 10000000000L;
         }
 
         {
             float floatVar = 0.0f;
+            floatVar = 3.14f;
+
             double doubleVar = 0.0;
+            doubleVar = 2.71828;
         }
 
         {
             char charVar = '\u0000';
+            charVar = 'A';
+
             String stringVar = "";
+            stringVar = "こんにちは";
         }
 
         {
             boolean booleanVar = false;
+            booleanVar = true;
         }
     }
 }
