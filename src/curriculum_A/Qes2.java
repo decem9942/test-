bade2dd9e6b5d884b9d990ecf9810846ab1f3765
cheng <cheng@decem>
2025-07-14 23:19:54 +0900

    test

---
+++ src/curriculum_A/Qes2.java
@@ -0,0 +1,16 @@
+package curriculum_A;
+
+public class Qes2 {
+	byte byteVar = 0;
+	short shortVar = 0;
+	int intVar = 0;
+	long longVar = 0L;
+
+	float floatVar = 0.0f;
+	double doubleVar = 0.0;
+
+	char charVar = '\u0000'; 
+	String stringVar = "a"; 
+	
+	boolean booleanVar = false;
+}
