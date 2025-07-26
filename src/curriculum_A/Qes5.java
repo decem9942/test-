2f7884e6992c49909092cc3372b3e458b853c82f
cheng <cheng@decem>
2025-07-26 22:34:27 +0900

    test

--- src/curriculum_A/Qes5.java
+++ src/curriculum_A/Qes5.java
@@ -1,5 +1,8 @@
 package curriculum_A;
 
 public class Qes5 {
-
+	String num = "20";
+	int num1 = 23;
+	System.ou.println("ハローJAVA" + (Integer.parseInt(num) + num1));
 }
+
---
+++ src/curriculum_A/Qes6.java
@@ -0,0 +1,22 @@
+package curriculum_A;
+
+public class Qes6 {
+	public class Main {
+	    public static void main(String[] args) {
+	        // 『山田太郎 18歳 170.5cm 62.2kg 寿司』をそれぞれ変数に代入
+	        String name = "山田太郎";
+	        int age = 18;
+	        double height = 170.5;
+	        double weight = 62.2;
+	        String food = "寿司";
+
+	        // format通りに出力
+	        System.out.println("初めまして" + name + "です");
+	        System.out.println("年齢は" + age + "歳です");
+	        System.out.println("身長は" + height + "cmです");
+	        System.out.println("体重は" + weight + "kgです");
+	        System.out.println("好きな食べ物は" + food + "です");
+	    }
+	    
+	}
+}
