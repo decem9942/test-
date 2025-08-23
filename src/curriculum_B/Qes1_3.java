05d096b3ecbb39e6881b3bc944b4131d872d4cb8
cheng <cheng@decem>
2025-08-24 04:39:44 +0900

    test

--- src/curriculum_B/Qes1_3.java
+++ src/curriculum_B/Qes1_3.java
@@ -3,18 +3,17 @@ package curriculum_B;
 import java.util.Scanner;
 
 public class Qes1_3 {
-     public static void main() {
-    	 Scanner scaner = new Scanner(System.in);
-    	 
-    	 System.out.print("ユーザー名を入力してください；");
-		String userame = scannr.nextLIne();
-    	 
-		 if (username == null || username.trim().isEmpty()) {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("ユーザー名を入力してください: ");
+	        String username = scanner.nextLine();
+
+	        if (username == null || username.trim().isEmpty()) {
 	            System.out.println("名前を入力してください");
 	        } else if (username.length() > 10) {
 	            System.out.println("名前を10文字以内にしてください");
 	        } else {
-	            System.out.println("ユーザー名「" + username + "」を登録しました");
+	            System.out.println("ユーザー名「 " + username + " 」を登録しました");
 	        }
 
 	        scanner.close();
