522b265aa233f241347a136bd3a793dd6611e5c3
cheng <cheng@decem>
2025-09-04 23:01:54 +0900

    test

--- src/curriculum_B/Qes4.java
+++ src/curriculum_B/Qes4.java
@@ -1,5 +1,12 @@
 package curriculum_B;
 
 public class Qes4 {
-
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++){
+			System.out.println(i);	
+			
+		for (int a = 1; a < 10; a++){
+				System.out.println(a);	
+	}
+}}
 }
